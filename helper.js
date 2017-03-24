b8a33675c10492d2bec82827e683693383d18fa2
Helcon Mabesa <helcon.mabesa@foxnews.com>
2017-03-24 11:35:10 -0400

    helpers

---
+++ helper.js
@@ -0,0 +1,168 @@
+/*eslint-env es6, node */
+'use strict';
+
+const shell = require('shelljs');
+const stripAnsi = require('strip-ansi');
+const mkdirp = require('mkdirp');
+const fs = require('fs');
+const _ = require('lodash');
+
+const PARAM_ARGV = process.argv;
+const PARAM_ARGS = (() => {
+    const args = {};
+    PARAM_ARGV.forEach((val, index, array) => {
+        let sp = val.split('='),
+            param = sp[0] || '',
+            data = sp[1];
+        if (param.length > 0 && /^--/i.test(param) && typeof data !== 'undefined') {
+            data = (data === 'true') ? true : (data === 'false') ? false : data;
+            args[param.replace(/^--/i, '')] = data;
+        }
+    });
+    return args;
+})();
+
+/**
+ * @class Helper
+ */
+class Helper {
+
+    /**
+     * Get arguments passed using the `--` convention
+     *
+     * ```
+     * ./foo.js --foo=bar --bar=baz
+     * ```
+     * @param  {String} id argument key
+     */
+    getOpt(id) {
+        return (id) ? PARAM_ARGS[id] : undefined;
+    }
+
+    /**
+     * Get non key-value pair arguments
+     *
+     * ```
+     * ./foo.js bar baz
+     * ```
+     *
+     * @param  {String} id argument key
+     */
+    getOptArg(id) {
+        const ret = _.some(PARAM_ARGV, (arg) => {
+            return (arg === id) ? true : false;
+        });
+        return ret;
+    }
+
+    /**
+     * Returns all the raw arguments
+     */
+    getRawArgs() {
+        return PARAM_ARGV || [];
+    }
+
+    /**
+     * Run a unix shell command using **[shelljs](https://www.npmjs.com/package/shelljs)**
+     *
+     * @param  {String} cmd command to run
+     * @param  {String} basePath (optional) path where to execute. if not defined, command executes on current dir
+     * @param  {Boolean} verbose (optional) non-silent mode
+     * @return String command output
+     */
+    shellCmd(cmd, basePath, verbose) {
+        const cmdArr = [];
+        if (basePath) {
+            cmdArr.push("cd " + basePath);
+        }
+        cmdArr.push(cmd);
+        const res = shell.exec(cmdArr.join(" && "), { silent: (verbose) ? false : true });
+        return stripAnsi((res.output || res.stdout || "").trim());
+    }
+
+    /**
+     * Check if path exists, either if its a file or directory
+     *
+     * @param  {String} path path to file/directory
+     * @return Boolean
+     */
+    isPathExists(path) {
+        let stat = true;
+        try {
+            stat = fs.existsSync(path);
+        } catch(err1) {
+            try {
+                stat = fs.statSync(path);
+            } catch(err2) {
+                stat = false;
+            }
+        }
+        return stat;
+    }
+
+    /**
+     * Check if the path is a file
+     *
+     * @param  {String} path path to file
+     * @return Boolean
+     */
+    isFileExists(path) {
+        let ret = false;
+        if (path && this.isPathExists(path)) {
+            const stat = fs.statSync(path);
+            if (stat.isFile()) {
+                ret = true;
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * Creates a directory
+     *
+     * @param  {String} path target path
+     */
+    createDir(path) {
+        if (!this.isPathExists(path)) {
+            mkdirp.sync(path, { encoding: 'utf8' });
+        }
+    }
+
+    /**
+     * Read file (utf-8)
+     *
+     * @param  {String} path path to utf-8 file
+     * @return String
+     */
+    readFile(path) {
+        let ret = false;
+        if (this.isPathExists(path)) {
+            ret = fs.readFileSync(path, { encoding: 'utf8' });
+        }
+        return ret;
+    }
+
+    /**
+     * Write file (utf-8)
+     *
+     * @param  {String} path target file path
+     * @param  {String} content file contents
+     */
+    writeFile(path, content) {
+        fs.writeFileSync(path, content, { encoding: 'utf8' });
+    }
+
+    /**
+     * Base64 encode/decode
+     *
+     * @param  {String}  str string contents
+     * @param  {Boolean} [dec=false] decode/encode mode (defaults to encode)
+     * @return String
+     */
+    base64(str, dec = false) {
+        return (dec) ? (new Buffer(str, 'base64').toString('ascii')) : (new Buffer(str).toString('base64'));
+    }
+
+}
+
+module.exports = new Helper;
---
+++ readme.md
@@ -0,0 +1,145 @@
+# File I/O Generic Helpers
+
+Generic helper functions used for development.
+
+**Install:**
+
+```bash
+npm install cli-helper
+```
+
+**Include:**
+
+```js
+var Helper = require('cli-helper');
+```
+
+## Functions
+
+* **`Helper.getOpt([String:id])`**
+
+Get arguments passed using the `--` convention.
+
+```js
+// execution
+node ./foo.js --foo=bar --bar=baz
+
+// getOpt()
+Helper.getOpt('foo');
+// -> 'bar'
+```
+
+* **`Helper.getOptArg([String:id])`**
+
+Get non key-value pair arguments
+
+```js
+// execution
+node ./foo.js foo
+
+// getOptArg()
+Helper.getOptArg('foo');
+// -> true
+
+Helper.getOptArg('bar');
+// -> false
+```
+
+* **`Helper.getRawArgs()`**
+
+Returns all the raw arguments.
+
+```js
+// execution
+node ./foo.js foo --bar=lol
+
+// getRawArgs()
+Helper.getOptArg('foo');
+// -> ['node', './foo.js', 'foo', '--bar=lol']
+```
+
+* **`Helper.shellCmd([String:cmd], [String:basePath], [Boolean:verbose])`**
+
+Run a unix shell command using package: **[shelljs](https://www.npmjs.com/package/shelljs)**
+
+
+| Arguments | Description |
+|:----------|:------------|
+| **cmd** | command to run |
+| **basePath** | (optional) path where to execute. if not defined, command executes on current dir |
+| **verbose** | (optional) non-silent mode |
+
+
+```js
+// shellCmd() - command ls -al output in /usr/local directory
+Helper.shellCmd('ls -al', '/usr/local');
+```
+
+* **`Helper.isPathExists([String:path])`**
+
+Check if path exists, either if its a **file or directory**.
+
+```js
+// isPathExists()
+Helper.isPathExists('/urs/local');
+// -> true
+```
+
+* **`Helper.isFileExists([String:path])`**
+
+Check if the path is a **FILE**.
+
+
+```js
+// isPathExists()
+Helper.isFileExists('/urs/local');
+// -> false
+
+Helper.isFileExists('/urs/local/foo.txt');
+// -> true
+```
+
+* **`Helper.createDir([String:path])`**
+
+Creates a directory.
+
+```js
+// createDir()
+Helper.createDir('/usr/local/new-dir');
+```
+
+* **`Helper.readFile([String:path])`**
+
+Read file (utf-8).
+
+```js
+// readFile()
+Helper.readFile('/usr/local/foo.txt');
+// -> {String} contents
+```
+
+* **`Helper.writeFile([String:path], [String:content])`**
+
+Write file (utf-8).
+
+```js
+// writeFile()
+Helper.writeFile('/usr/local/foo.txt', 'hello world!');
+// -> {String} contents
+```
+
+* **`Helper.base64([String:str], [Boolean:dec = false])`**
+
+Base64 encode/decode.
+
+```js
+// base64()
+
+// encode
+Helper.base64('encode me');
+// -> {String} encoded string
+
+// decode
+Helper.base64('ZW5jb2RlIG1l', true);
+// -> {String} decoded string
+```
